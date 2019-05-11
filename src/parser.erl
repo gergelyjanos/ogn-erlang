5043a29ec719d81fb49b512b92656dd95f7a929a
Janos Gergely <gergelyjanos@td1990.hu>
2019-05-11 19:40:35 +0200

    Parse receiver

--- src/parser.erl
+++ src/parser.erl
@@ -7,27 +7,65 @@
 -export([run/0]).
 
 -define(beaconData, Device, MessageFormat, Receiver, Timestamp, Latitude, Longitude, Heading, GroundSpeed, Altitude, Comment).
+-define(receiverPosition, Receiver, MessageFormat, Server, Timestamp, Latitude, Longitude, Altitude).
+-define(receiverStatus, Receiver, MessageFormat, Server, Timestamp, Version, Cpu, Ram, Other).
+-define(AIRCRAFT_PATTERN, "^(?P<device>.+)>(?P<message_format>.+),.+,(?P<receiver>.+):\\/(?P<timestamp>\\d+)h(?P<latitude>\\d+\\.\\d+[NS])[\\/\\\\]+(?P<longitude>\\d+\\.\\d+[EW])(?P<heading>\\S\\d+)\\/(?P<ground_speed>\\d+)\\/A=(?P<altitude>\\d+)\\s(?P<comment>.*)$").
+-define(RECEIVERPOSITION_PATTERN, "^(?P<receiver>.+)>(?P<message_format>.+),.+,.+,(?P<server>.+):\\/(?P<timestamp>\\d+)h(?P<latitude>\\d+\\.\\d+[NS])[\\/\\\\I]+(?P<longitude>\\d+\\.\\d+[EW])[\\/\\&]+A=(?P<altitude>\\d+)$").
+-define(RECEIVERSTATUS_PATTERN, "^(?P<receiver>.+)>(?P<message_format>.+),.+,.+,(?P<server>.+):>(?P<timestamp>\\d+)h\\s+(?P<version>.+)\\s+CPU:(?P<cpu>\\d+\\.\\d+)\s+RAM:(?P<ram>\\S+)\\s+(?P<other>.*)$").
+
 %% @doc Public function to parse messages.
 run() ->
     receive 
-        {line, Line} -> parseline(string:chomp(Line)), run();
+        {line, Line} -> parseline(Line), run();
         {comment, Comment} -> parsecomment(Comment), run();
         {close} -> io:format("Close parser~n")
     end
 .	
 
-%% @doc Private function to parse line.
-parseline(Line) ->
-    % {ok, PATTERN_APRS} = re:compile("^(?P<callsign>.+?)>(?P<dstcall>[A-Z0-9]+),((?P<relay>[A-Za-z0-9]+)\*)?.*,(?P<receiver>.+?):(?P<aprs_type>(/|>))(?P<aprs_body>.*)$"),
-    {ok, PATTERN_Aircraft} = re:compile("^(?P<device>.+)>(?P<message_format>.+),.+,(?P<receiver>.+):\\/(?P<timestamp>\\d+)h(?P<latitude>\\d+\\.\\d+[NS])[\\/\\\\]+(?P<longitude>\\d+\\.\\d+[EW])['^](?P<heading>\\d+)\\/(?P<ground_speed>\\d+)\\/A=(?P<altitude>\\d+)\\s(?P<comment>.*)$", [unicode]),
-    case re:run(Line, PATTERN_Aircraft, [{capture, all_but_first, list}]) of 
-        {match, [?beaconData]} ->
-            io:format("Line parser match ~p~n", [[?beaconData]])
-        ;
-        nomatch -> 
-            io:format("Line parser nomatch ~p~n", [Line])
+parseline(Line) -> 
+    ParseStatus = parseline(aircraft, string:chomp(Line)),
+    if 
+        ParseStatus == nomatch -> 
+            io:format("Line parser nomatch ~p~n", [Line]);
+        true -> true
     end
-.	
+.
+
+%% @doc Private function to parse line.
+parseline(aircraft, Line) ->
+    {ok, AircraftRegex} = re:compile(?AIRCRAFT_PATTERN, [unicode]),
+    case re:run(Line, AircraftRegex, [{capture, all_but_first, list}]) of 
+        {match, [?beaconData]} ->
+            % todo parse more data from comment
+            % todo send aircraft data to DB
+            io:format("Aircraft match ~p~n", [[Device, Receiver, Latitude, Longitude, Heading, GroundSpeed]]),
+            match;
+        nomatch ->
+            parseline(receiverPosition, Line)
+    end
+;
+parseline(receiverPosition, Line) ->
+    {ok, Regex} = re:compile(?RECEIVERPOSITION_PATTERN, [unicode]),
+    case re:run(Line, Regex, [{capture, all_but_first, list}]) of 
+        {match, [?receiverPosition]} ->
+            % todo send receiver position data to DB
+            io:format("Receiver position match ~p~n", [[Receiver]]),
+            match;
+        nomatch -> 
+            parseline(receiverStatus, Line)
+    end
+;
+parseline(receiverStatus, Line) ->
+    {ok, Regex} = re:compile(?RECEIVERSTATUS_PATTERN, [unicode]),
+    case re:run(Line, Regex, [{capture, all_but_first, list}]) of 
+        {match, [?receiverStatus]} ->
+            % todo send receiver status data to DB
+            io:format("Receiver status match ~p~n", [[Receiver, Cpu, Ram]]),
+            match;
+        nomatch -> 
+            nomatch
+    end
+.
 
 %% @doc Private function to parse comment.
 parsecomment(Comment) ->
