1110eda93f81a0305847139914f1c516bc3f393d
Janos Gergely <gerge@janosyogax1.fahaz.td1990.intra>
2020-12-08 15:12:41 +0100

    Remove old codes

--- src/old_codes/deployhelper.erl
+++
@@ -1,9 +0,0 @@
-%% @author Janos Gergely <gergelyjanos@td1990.org>
-%% @doc Module to compile and create docs and etc.
-%% @copyright 2019 TD*1990
-%% @version 1.0.0
-
--module(deployhelper).
--export([docs/0]).
-
-docs() -> edoc:files(["deployhelper.erl", "startup.erl"], [{dir, "../doc"}]).
